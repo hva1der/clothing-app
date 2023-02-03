90d3fd03180027f1ab184d54385e2399a97650c5
Henning Valderhaug <hvalderhaug@gmail.com>
2023-02-03 18:00:24 +0000

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+function Header() {
+  return (
+    <img
+      src="https://e7.pngegg.com/pngimages/956/295/png-clipart-mr-pickles-season-1-mr-pickles-season-2-loose-tooth-pickled-cucumber-animation-others-television-food-thumbnail.png"
+      alt="Mr Pickles logo"
+    />
+  );
+}
+
+export default Header;
