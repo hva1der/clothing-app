da644be78653a959efb8739f3b0cba9d42c7d374
Henning Valderhaug <hvalderhaug@gmail.com>
2023-02-03 21:22:02 +0000

    for sharing with lards

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,27 @@
-// import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function Header() {
   return (
-    <img
-      src="https://e7.pngegg.com/pngimages/956/295/png-clipart-mr-pickles-season-1-mr-pickles-season-2-loose-tooth-pickled-cucumber-animation-others-television-food-thumbnail.png"
-      alt="Mr Pickles logo"
-    />
+    <Container>
+      <Row>
+        {/* Logo goes here */}
+        <Col md={2}>
+          <Image
+            fluid
+            roundedCircle
+            src="https://e7.pngegg.com/pngimages/956/295/png-clipart-mr-pickles-season-1-mr-pickles-season-2-loose-tooth-pickled-cucumber-animation-others-television-food-thumbnail.png"
+            alt="Mr Pickles logo"
+          />
+        </Col>
+        {/* Header text goes here */}
+        <Col md={2}>
+          <h1>This is the header</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
